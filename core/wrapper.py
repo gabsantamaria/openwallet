a4ebc66cd2082fdedc74556b10ebc3f4c3e2b722
Gabriel <gabriel19916@gmail.com>
2018-03-11 23:21:45 -0400

    Add signtxn function and fix elefork bugs

--- core/wrapper.py
+++ core/wrapper.py
@@ -6,13 +6,6 @@ from pathlib import Path
 Config = namedtuple('Config', 'binary_cmd, default_wdir, max_wid')
 conf = Config("elefork", str(Path(os.getcwd()).parents[1]), 100)
 
-def get_free_wid(wdir = ""):
-	global conf
-	for wid in range(1,conf.max_wid + 1):
-		if not os.path.exists(get_full_wpath(wid, wdir)):
-			return wid
-	return 0
-
 def run(args):
 	return subprocess.run(args, stdout=subprocess.PIPE)
 
@@ -22,6 +15,13 @@ def get_full_wpath(wid, wdir):
 		return conf.default_wdir + "/wallet" + str(wid)
 	else:
 		return wdir + "/wallet" + str(wid)
+
+def get_free_wid(wdir = ""):
+	global conf
+	for wid in range(1,conf.max_wid + 1):
+		if not os.path.exists(get_full_wpath(wid, wdir)):
+			return wid
+	return 0
 
 def create_seed():
 	global conf
@@ -54,3 +54,19 @@ def get_mpk(wid, wdir = ""):
 		return mpk[2:-3]
 	else:
 		return ""
+
+def sign_transaction(unsigned_txn, wid, pwd = None, wdir = ""):
+	global conf
+	comm = ['cat', '/home/gsant/Dropbox/JAG_LY/walletside/openwallet/core/unsigned.txn', '|', 'elefork','signtransaction', '-w', get_full_wpath(wid, wdir)]
+	if pwd != None:
+		comm.append("-encpwd")
+		comm.append(str(pwd))
+	comm.append('->')
+	comm.append('/home/gsant/Dropbox/JAG_LY/walletside/openwallet/core/signed.txn')
+	#cat = subprocess.Popen(('cat', 'unsigned.txn'), stdout=subprocess.PIPE)
+	#output = subprocess.check_output(comm, stdin=cat.stdout)
+	#cat.wait()
+	#comm = "cat /home/gsant/Dropbox/JAG_LY/walletside/openwallet/core/unsigned.txn | elefork signtransaction -w " + get_full_wpath(wid, wdir) + " -encpwd 1520 -> /home/gsant/Dropbox/JAG_LY/walletside/openwallet/core/signed.txn"
+	comm = " ".join(comm)
+	cat = subprocess.Popen(comm, shell=True, stdout=subprocess.PIPE)
+	print(cat.stdout)
