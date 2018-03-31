7593ed6c3764ace40baebe1c8aff244b6e7535db
Gabriel <gabriel19916@gmail.com>
2018-03-31 10:49:55 -0400

    fix bugs

--- core/workflow.py
+++ core/workflow.py
@@ -63,7 +63,7 @@ def wait_for_connection():
 	signed_txn = -1
 	trials_left = 3
 	scrambled = [0,1,2,3,4,5,6,7,8,9]
-	print("original scrambled: ", scrambled)
+	#print("original scrambled: ", scrambled)
 	while signed_txn == -1 and trials_left > 0:
 		random.shuffle(scrambled)
 		print("now scrambled: ", scrambled)
